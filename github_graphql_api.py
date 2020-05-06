5ac97e4c2fd0897df5d1e88bad8c9542e48082ab
Nancy Chauhan <nancychn1@gmail.com>
2020-05-06 16:08:58 +0530

    Add script to play using Github Graphql API.

---
+++ github_graphql_api.py
@@ -0,0 +1,39 @@
+import requests
+
+headers = {"Authorization": "token <GITHUB_TOKEN>"}
+
+
+def run_query(query): # A simple function to use requests.post to make the API call. Note the json= section.
+    request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+
+        
+# The GraphQL query (with a few aditional bits included) itself defined as a multi-line string.       
+query = """
+{
+  repository(name: "<NAME>", owner: "<OWNER>") {
+    pullRequest(number: 4172) {
+      commits(last: 1) {
+        nodes {
+          commit {
+            status {
+              state
+              contexts {
+                context
+                description
+                state
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+"""
+
+result = run_query(query) # Execute the query
+print(result)
